c1e01b3d657e23b82fea53937ca99af37664718d
WisniewskiGabriel <gabriel_wisniewski@gmail.com>
2022-11-30 20:14:57 -0300

    atividade 1 pronta

---
+++ EX_01/index.js
@@ -0,0 +1,44 @@
+let voteValue = 0;
+let votesTotal = 0;
+let qtdVotos13 = 0;
+let qtdVotos22 = 0;
+let qtdVotosNulos = 0;
+let voteIsAttributed = false;
+let displayVotes13 = document.getElementById("display13");
+let displayVotes22 = document.getElementById("display22");
+let displayVotesNulos = document.getElementById("displayNulos");
+let voteSelector = document.getElementById("num-candidato");
+
+function pegarVoto(){
+
+    voteIsAttributed = false;
+    voteValue = voteSelector.value;
+    voteSelector.value = '';
+    voteValue = Number(voteValue);
+    votesTotal = votesTotal+1;
+
+    if (voteValue === 13 && !voteIsAttributed) {
+        qtdVotos13 = qtdVotos13+1;
+        voteIsAttributed = true;
+    }
+    if (voteValue === 22 && !voteIsAttributed) {
+        qtdVotos22 = qtdVotos22+1;
+        voteIsAttributed = true;
+    }
+
+    if(!voteIsAttributed){
+        qtdVotosNulos = qtdVotosNulos+1;
+    }
+
+    console.log("Total: "+votesTotal+"\n13: "+qtdVotos13+"\n22: "+qtdVotos22+"\nNulos: "+qtdVotosNulos);
+    
+    if (qtdVotos13 > 0){
+        displayVotes13.textContent = ((qtdVotos13*100)/votesTotal).toFixed(2)+"%";
+    }
+    if (qtdVotos22 > 0){
+        displayVotes22.textContent = ((qtdVotos22*100)/votesTotal).toFixed(2)+"%";
+    }
+    if (qtdVotosNulos > 0){
+        displayVotesNulos.textContent = ((qtdVotosNulos*100)/votesTotal).toFixed(2)+"%";
+    }
+}
