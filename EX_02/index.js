0ef6142e727487d1ead62c264c5a52949390bd7f
WisniewskiGabriel <gabwisniewski@gmail.com>
2022-12-02 23:02:17 -0300

    atividade 2 pronta

--- EX_02/index.js
+++ EX_02/index.js
@@ -2,26 +2,93 @@ let elementResultados = document.getElementById("results");
 let btnCalcular = document.getElementById("btnCalcular");
 let elementInput = document.getElementById("input-horas");
 let showResults = false;
-let minWage = 1000;
-let weeksInMonth = 4;
-let maxAmountRegularHours = 40;
-let regularHours;
-let overtimeHours;
-let workedHours;
+let hourlySalary = 1;
+let overtimeRaisePercentage = 50;
+let maxAmountRegularHours = (40*4);
+let displayHourlySalary = document.getElementById("hourly-salary");
+let displayTotalHours = document.getElementById("display-total-hours");
+let displayRegularSalary = document.getElementById("display-regular-salary");
+let displayOvertimeSalary = document.getElementById("display-overtime-salary");
+let displayTotalSalary = document.getElementById("display-total-salary");
 
 
 hideResults();
+btnCalcular.addEventListener('click',doAllTasks);
 
-btnCalcular.addEventListener('click',getWorkedHours);
+function doAllTasks(){
+
+    let integerIsValid = false;
+    let objResults;
+    let workedHours;
+
+    workedHours = getWorkedHours();
+    console.log(workedHours);
+    integerIsValid = checkIntegerValidity(workedHours);
+    emptyInput(elementInput);
+    
+    if(!integerIsValid){
+        window.location.reload();
+        throw "ERRO NO INPUT";
+    }
+
+    objResults = organizeValues(workedHours,maxAmountRegularHours,hourlySalary,overtimeRaisePercentage);
+    console.log(objResults);
+
+    displayHourlySalary.textContent = `R\$${hourlySalary.toFixed(2)}`;
+    displayTotalHours.textContent = workedHours;
+    displayRegularSalary.textContent = objResults.regularSalary;
+    displayOvertimeSalary.textContent = objResults.overtimeSalary;
+    displayTotalSalary.textContent = objResults.totalSalary;
+
+    exhibitResults();
+
+
+}
 
 function getWorkedHours(){
     workedHours = elementInput.value;
-    console.log(workedHours);
     return workedHours;
 }
 
-function calculateValues(){
-    
+function organizeValues(workedHours, maxAmountRegularHours, hourlySalary, overtimeRaisePercentage){
+
+    let regularHours = 0;
+    let overtimeHours = 0;
+    let objResults;
+    let regularSalary = 0;
+    let overtimeSalary = 0;
+    let totalSalary = 0;
+    workedHours = Number(workedHours);
+    maxAmountRegularHours = Number(maxAmountRegularHours);
+
+    if(workedHours >= maxAmountRegularHours){
+        regularHours = maxAmountRegularHours;
+        workedHours = Number(workedHours)-Number(regularHours);
+        overtimeHours = workedHours;
+    }
+
+    if(workedHours < maxAmountRegularHours){
+        regularHours = workedHours;
+        workedHours = Number(regularHours)-Number(workedHours);
+        overtimeHours = workedHours;
+    }
+
+    regularSalary = (regularHours * hourlySalary);
+    overtimeSalary = overtimeHours * (((hourlySalary/100)*overtimeRaisePercentage)+(hourlySalary));
+    totalSalary = overtimeSalary+regularSalary;
+
+    regularSalary = transformIntoBeautifulMonetaryString(regularSalary);
+    overtimeSalary = transformIntoBeautifulMonetaryString(overtimeSalary);
+    totalSalary = transformIntoBeautifulMonetaryString(totalSalary);
+
+    objResults =    {'regularHrs':regularHours,
+                     'overtimeHrs':overtimeHours,
+                     'regularSalary':regularSalary,
+                     'overtimeSalary': overtimeSalary,
+                     'totalSalary':totalSalary};
+
+    return objResults;
+
 }
 
 function hideResults(){
@@ -50,3 +117,41 @@ function showResultsVisibility(showResults){
     elementResultados.style.visibility = strVisibilityValue;
 }
 
+function checkIntegerValidity(intValue){
+
+    let integerIsValid=false;
+    let errorAlertIsDeployed;
+
+    if(!intValue.length>0 && !errorAlertIsDeployed){
+        integerIsValid=false;
+        errorAlertIsDeployed=true;
+        deployErrorAlert("O input estava vazio. Digite algo.");
+    }
+    if(isNaN(intValue) && !errorAlertIsDeployed){
+        integerIsValid=false;
+        errorAlertIsDeployed=true;
+        deployErrorAlert("O input não era numérico.");
+    }
+
+    if(!errorAlertIsDeployed){
+        integerIsValid = true;
+    }
+
+    return integerIsValid;
+
+}
+
+function deployErrorAlert(strError){
+    alert("Aqui deveria haver um modal que diz: \n"+strError);
+}
+
+function emptyInput(elemento){
+    elemento.value = '';
+}
+
+function transformIntoBeautifulMonetaryString(intValue){
+    
+    let strValue = "R$ "+intValue.toFixed(2);
+    return strValue;
+    
+}
